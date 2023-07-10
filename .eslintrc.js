a4cc48330d37cf3bbea070b21250ae6b165ef452
Jaewook Ahn <dev.jaewook@gmail.com>
2023-07-10 18:59:46 +0900

    chore: apply prettier on eslint
    - add eslint-plugin-prettier
    - add eslint-config-prettier
    - update .prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   overrides: [
     {
       env: {
